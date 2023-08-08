f7c45c134d5387842d9ccfbc00de6b1894934d1b
Justin Allard <jallard@hbs.net>
2023-08-08 14:49:02 -0500

    Init Commit

---
+++ pages/index.js
@@ -0,0 +1,49 @@
+import Head from "next/head";
+import Hero from "../components/hero";
+import Navbar from "../components/navbar";
+import SectionTitle from "../components/sectionTitle";
+import { benefitOne, benefitTwo } from "../components/data";
+import Benefits from "../components/benefits";
+import Footer from "../components/footer";
+import Cta from "../components/cta";
+import Faq from "../components/faq";
+import PopupWidget from "../components/popupWidget";
+
+const Home = () => {
+  return (
+    <>
+      <Head>
+        <title>Car Shows WI</title>
+        <meta
+          name="description"
+          content="Nextly is a free landing page template built with next.js & Tailwind CSS"
+        />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <Navbar />
+      <Hero />
+      <SectionTitle
+        pretitle="Nextly Benefits"
+        title=" Why should you use this landing page">
+        Nextly is a free landing page & marketing website template for startups
+        and indie projects. Its built with Next.js & TailwindCSS. And its
+        completely open-source.
+      </SectionTitle>
+      <Benefits data={benefitOne} />
+      <Benefits imgPos="right" data={benefitTwo} />
+
+
+      <SectionTitle pretitle="FAQ" title="Frequently Asked Questions">
+        Answer your customers possible questions here, it will increase the
+        conversion rate as well as support or chat requests.
+      </SectionTitle>
+      <Faq />
+      <Cta />
+      <Footer />
+      <PopupWidget />
+    </>
+  );
+}
+
+export default Home;
