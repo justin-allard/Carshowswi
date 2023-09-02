56cae3b6167ae5cac28c0b5cb1b9c81681fe0a56
Justin Allard <justinallard@icloud.com>
2023-09-02 12:24:37 -0500

    Update events.js

--- components/events.js
+++ components/events.js
@@ -11,12 +11,11 @@ const Events  = () => {
     <div>
       <img src="https://images.unsplash.com/photo-1497493292307-31c376b6e479?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1171&q=80" alt=""/>
       <div>
-        <h1  class="mt-3 text-gray-800 text-2xl font-bold my-2">long established</h1>
-        <p class="text-gray-700 mb-2">It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that....</p>
-        <div class="flex justify-between mt-4">
-          <span class="font-thin text-sm">May 20th 2020</span>
-          <span class="mb-2 blue-text font-bold">Read more</span>
-        </div>
+        <h1  class="mt-3 text-white-800 text-2xl font-bold my-2">Car Show Title</h1>
+        <p class="text-white-700 mb-2 blue-text">May 20th 2020</p>
+        <p class="text-white-700 mb-2">Adams, WI</p>
+        <p class="text-white-700 mb-2">It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that....</p>
+        
       </div>
     </div>
     <div>
